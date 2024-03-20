59346eb8ae025e352e95bbd345d0d860d299f7f0
you06 <you1474600@gmail.com>
2024-03-20 21:20:01 +0900

    update icon
    Signed-off-by: you06 <you1474600@gmail.com>

--- src/lib/endpoint.ts
+++ src/lib/endpoint.ts
@@ -1,10 +1,10 @@
-import axios, {isCancel, AxiosError} from 'axios'
+import axios from 'axios'
 
 const ENDPOINT_ROOT: string = 'https://ap-northeast-1.data.tidbcloud.com/api/v1beta/app/dataapp-dgUIdIWT/endpoint/v1/'
 const USERNAME: string = 'K13PSIV0'
 const PASSWORD: string = 'c3872e69-297c-451f-bb27-184dff738f28'
 
-let request = async (path: string) => {
+let request = async (path: string, params?: {[key: string]: string}) => {
     const url = ENDPOINT_ROOT + path;
     try {
         const response = await axios.get(url, {
@@ -12,7 +12,8 @@ let request = async (path: string) => {
             auth: {
                 username: USERNAME,
                 password: PASSWORD
-            }
+            },
+            params,
         });
         return response.data.data;
     } catch (error) {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,15 +4,6 @@ import { request } from '$lib/endpoint'
 export async function load({}) {
     const background_resp = await request('background')
 	return {
-		posts: [
-            {id: 'post-1'},
-            {id: 'post-2'},
-            {id: 'post-3'},
-            {id: 'post-4'},
-            {id: 'post-5'},
-            {id: 'post-6'},
-            {id: 'post-7'},
-        ],
         background: background_resp.rows[0]
 	};
 }
